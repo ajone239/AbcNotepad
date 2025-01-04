dc6b110d51e46941d756c1e71ecaab52ea0bb56d
Austin Jones <ajones53.aj@gmail.com>
2025-01-04 00:38:07 -0500

    works shoddily

--- app/(tabs)/data.tsx
+++ app/(tabs)/data.tsx
@@ -1,23 +1,31 @@
-import { StyleSheet, Text, View } from "react-native"
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, selectCount } from '../../src/counterSlice';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 export default function AboutScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Data Screen
-      </Text>
-    </View>
-  );
+    const count = useSelector(selectCount)
+    const dispatch = useDispatch();
+
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Redux Example</Text>
+            <Text>Count: {count}</Text>
+            <Button title="Increment" onPress={() => dispatch(increment())} />
+            <Button title="Decrement" onPress={() => dispatch(decrement())} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#25292e',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#fff',
+    },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,15 @@
+import { Provider } from "react-redux"
 import { Stack } from "expo-router";
+import { store } from "../src/store"
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" />
-    </Stack>
-  );
+    return (
+        <Provider store={store}>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="+not-found" />
+            </Stack>
+        </Provider>
+    );
 }
 
