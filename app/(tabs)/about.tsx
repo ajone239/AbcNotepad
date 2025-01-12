9b62d56f7e8c8a45dc8672a1c46db39efb99c0e6
Austin Jones <ajones53.aj@gmail.com>
2025-01-11 22:54:51 -0500

    lint

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -5,8 +5,6 @@ import React from 'react';
 import { StyleSheet, View, Text, Pressable, } from 'react-native';
 import { useDispatch } from 'react-redux';
 
-
-
 export default function AboutScreen() {
     const dispatch = useDispatch()
 
