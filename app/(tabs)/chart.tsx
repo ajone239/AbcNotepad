3655f079cd9e98c1bce27edaf34ff8341dacac42
Austin Jones <ajones53.aj@gmail.com>
2025-02-28 11:54:10 -0600

    random data in the chart

--- app/(tabs)/chart.tsx
+++ app/(tabs)/chart.tsx
@@ -1,17 +1,55 @@
 import { Theme } from '@/src/colors';
 import React from 'react';
 import {
+    Dimensions,
     StyleSheet,
     Text,
     View
 } from 'react-native';
 
+import { LineChart } from 'react-native-chart-kit'
+
 export default function Chart() {
+    const data = {
+        labels: ["January", "February", "March", "April", "May", "June"],
+        datasets: [
+            {
+                data: [
+                    Math.random() * 100,
+                    Math.random() * 100,
+                    Math.random() * 100,
+                    Math.random() * 100,
+                    Math.random() * 100,
+                    Math.random() * 100
+                ]
+            }
+        ]
+    };
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>
-                Charts are cool!
-            </Text>
+            <LineChart
+                data={data}
+                width={Dimensions.get("window").width} // from react-native
+                height={Dimensions.get("window").height}
+                chartConfig={{
+                    backgroundColor: Theme.background,
+                    backgroundGradientFrom: Theme.background,
+                    backgroundGradientTo: Theme.background,
+                    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                    propsForDots: {
+                        r: "6",
+                        strokeWidth: "2",
+                        stroke: Theme.text,
+                    }
+                }}
+                bezier
+                style={{
+                    marginVertical: 8,
+                    borderRadius: 10,
+                }}
+            />
         </View>
     );
 }
